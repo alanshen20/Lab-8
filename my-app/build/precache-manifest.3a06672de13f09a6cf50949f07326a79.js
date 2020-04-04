91df1beb6d8f6f53b03ed2c89fdd4aa8cb5fc5bc
alanshen20 <alanshen20@ufl.edu>
2020-04-04 23:55:55 +0000

    actually added build

---
+++ my-app/build/precache-manifest.3a06672de13f09a6cf50949f07326a79.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ac7f6980fe03f26bae30c0e83341f8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01296ee744a925588575",
+    "url": "/static/css/main.81a89d0a.chunk.css"
+  },
+  {
+    "revision": "602ae8c8cf9fb525be20",
+    "url": "/static/js/2.31365275.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.31365275.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01296ee744a925588575",
+    "url": "/static/js/main.1c0f02e0.chunk.js"
+  },
+  {
+    "revision": "228fa098814da32aa5c2",
+    "url": "/static/js/runtime-main.e1daef6b.js"
+  },
+  {
+    "revision": "d1723b66563707ea255745b81846fc91",
+    "url": "/static/media/Eric-Fuller.d1723b66.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
